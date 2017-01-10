c19ea3cd9ecb84f09c77748c8b00a441731d1730
Student6 <Student6@10.8.33.176>
2017-01-10 12:20:26 -0500

    1 10 17

---
+++ Introduction/src/PartnerCodeInHerePlease/Button.java
@@ -0,0 +1,67 @@
+package PartnerCodeInHerePlease;
+
+import java.awt.image.BufferedImage;
+
+import gui.simon.ButtonInterfaceAnna;
+
+public class Button implements ButtonInterfaceAnna {
+
+	public Button() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean isHovered(int x, int y) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void act() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public BufferedImage getImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isAnimated() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
