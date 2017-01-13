7c7125e50f42c4e983c0c609778abecb49f64594
Student6 <Student6@10.8.33.176>
2017-01-13 12:33:44 -0500

    1 13 17

--- Introduction/src/gui/ClickableScreen.java
+++ Introduction/src/gui/ClickableScreen.java
@@ -1,5 +1,8 @@
 package gui;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
@@ -8,52 +11,22 @@ import java.util.List;
 import gui.components.Clickable;
 import gui.components.Visible;
 
-public abstract class ClickableScreen extends Screen implements MouseListener {
-	
-	private ArrayList<Clickable> clickables;
 
+
+
+public abstract class ClickableScreen extends Screen implements MouseListener {
+
+	private ArrayList<Clickable> clickables;
+	
 	public ClickableScreen(int width, int height) {
 		super(width, height);
 		// TODO Auto-generated constructor stub
 	}
+
+	public abstract void initAllObjects(List<Visible> viewObjects);
 	
-	public MouseListener getMouseListener(){
-		return this;
-	}
-
-	@Override
-	public void mouseClicked(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseExited(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mousePressed(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
 	@Override
 	public void initObjects(ArrayList<Visible> viewObjects) {
-		// TODO Auto-generated method stub
 		initAllObjects(viewObjects);
 		clickables = new ArrayList<Clickable>();
 		for(Visible v: viewObjects){
@@ -63,19 +36,51 @@ public abstract class ClickableScreen extends Screen implements MouseListener {
 		}
 	}
 
-	public abstract void initAllObjects(List<Visible> viewObjects);
+	public void mouseClicked(MouseEvent m) {
+		for(Clickable c: clickables){
+			if(c.isHovered(m.getX(), m.getY())){
+				c.act();
+				break;
+			}
+		}
+	}
+	
+	public void addObject(Visible v){
+		super.addObject(v);
+		if(v instanceof Clickable){
+			clickables.add((Clickable)v);
+		}
+	}
+	
 
-	 public void addObject(Visible v){
-		 super.addObject(v);
-		 if(v instanceof Clickable){
-		 clickables.add((Clickable)v);
-		 }
-	 }
-		 
-
-		 
+	
 	public void remove(Visible v){
-		 super.remove(v);
-		 clickables.remove((Clickable)v);
-	} 
+		super.remove(v);
+		clickables.remove(v);
+	}
+
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public MouseListener getMouseListener(){
+		return this;
+	}
+	
 }
